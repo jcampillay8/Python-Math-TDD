a52fadb5680a23307df50368bce6db883c6f166f
Jaime Campillay <jcampillayworks@gmail.com>
2021-04-17 11:32:09 -0400

    3 test TDD

--- math_dojo_tdd.py
+++ math_dojo_tdd.py
@@ -5,13 +5,15 @@ class MathTest(unittest.TestCase):
     def setUp(self):
         self.md = MathDojo()
 
-    def testAdd(self):
+    def test1(self):
         self.assertEqual(self.md.add([2,3,4]).add(2,5,1).subtract(3,[3,4]).result,7)
 
-    def testSubstract(self):
+    def test2(self):
         self.assertEqual(self.md.add([2,2],[5,3]).add(2,5,(1,2,3)).subtract(3,2,(1,2,3)).result, 14)
   
+    def test3(self):
+        self.assertGreater(self.md.add([2,2],[5,3]).add(2,5,(1,2,3)).subtract(3,2,(1,2,3)).result, 2)
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
 
